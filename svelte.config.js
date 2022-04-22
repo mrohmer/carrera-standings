3a63268f520a134f56544c853139c4227e3ac395
mrohmer <rohmer.matze@gmail.com>
2022-04-22 13:36:49 +0200

    build: fix prerendering

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import fs from "fs";
+import path from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +10,14 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			entries: [
+				'*',
+				...fs.readdirSync('./content/cups')
+				.map(file => `/api/cups/${path.basename(file, '.json')}`),
+			]
+		}
 	}
 };
 
