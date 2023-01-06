d085da3c04ea7dad21dd0e7e9a73cb33d476c12f
mrohmer <rohmer.matze@gmail.com>
2023-01-06 16:34:53 +0100

    fix: fix course highlight

--- src/routes/[[year]]/course/+layout.svelte
+++ src/routes/[[year]]/course/+layout.svelte
@@ -14,20 +14,22 @@
 	<ul
 		class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400"
 	>
-		{#each Object.entries(tabs) as [href, label]}
+		{#each Object.entries(tabs) as [path, label]}
+			{@const href = `${urlPrefix}/course${path}`}
+			{@const isActive = $page.url.pathname === `${urlPrefix}/course${path}`}
 			<li class="md:mr-2">
 				<a
-					href={`${urlPrefix}/course${href}`}
+					{href}
 					class="inline-block py-4 px-2 md:px-4 border-b-2 rounded-t-l transition-colors"
-					class:border-transparent={$page.url.pathname !== `/course${href}`}
-					class:hover:text-gray-600={$page.url.pathname !== `/course${href}`}
-					class:hover:border-gray-300={$page.url.pathname !== `/course${href}`}
-					class:dark:hover:text-gray-300={$page.url.pathname !== `/course${href}`}
-					class:border-red-600={$page.url.pathname === `/course${href}`}
-					class:text-red-600={$page.url.pathname === `/course${href}`}
-					class:dark:text-red-500={$page.url.pathname === `/course${href}`}
-					class:dark:border-red-500={$page.url.pathname === `/course${href}`}
-					aria-current={$page.url.pathname === `${urlPrefix}/course${href}` ? 'page' : undefined}
+					class:border-transparent={!isActive}
+					class:hover:text-gray-600={!isActive}
+					class:hover:border-gray-300={!isActive}
+					class:dark:hover:text-gray-300={!isActive}
+					class:border-red-600={isActive}
+					class:text-red-600={isActive}
+					class:dark:text-red-500={isActive}
+					class:dark:border-red-500={isActive}
+					aria-current={isActive ? 'page' : undefined}
 				>
 					{label}
 				</a>
