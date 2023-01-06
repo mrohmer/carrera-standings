9ed1bae70102b284176537892f8ed005ec96c44b
mrohmer <rohmer.matze@gmail.com>
2023-01-06 16:17:55 +0100

    fix: fix standings api route

--- src/routes/api/[year]/standings/+server.ts
+++ src/routes/api/[year]/standings/+server.ts
@@ -1,10 +1,14 @@
-import { readCupFiles } from '$lib/utils/read-content-files';
-import type { Standings } from '$lib/models';
+import { readCupFiles, readRacersFile } from '$lib/utils/read-content-files';
 import { standingsConverter } from '$lib/converters/standings.converter';
 import type { RequestHandler } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import { getYear } from '$lib/api/get-year';
 
 export const GET: RequestHandler = (event) => {
 	const year = getYear(event);
-	return new Response(JSON.stringify(standingsConverter(readCupFiles(year))));
+	const racers = readRacersFile(year);
+	if (!racers) {
+		throw error(404);
+	}
+	return new Response(JSON.stringify(standingsConverter(readCupFiles(year), racers)));
 };
